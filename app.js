bffb3a94bb56c0a99d0a909e64216bfa4a34d1eb
zxvnme <igoranonim1337@gmail.com>
2017-12-12 21:01:20 +0100

    Code style changes
    Changed:
    - Functions order (only visual effect).
    - Made code more transparent.
    - Added some JSDocs.

--- app.js
+++ app.js
@@ -6,21 +6,81 @@ const pack = require('./package.json');
 const steam = new steamapi({ apiKey: config.apikey, format: 'json' });
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-/**
- * Prints proper command usage to console with 'Invalid syntax!' prefix.
- * @constructor
- * @param {string} pusg - Proper usage. 
- */
-function syntaxError(pusg) {
-   return console.log('    Invalid syntax! Usage: ' + pusg);    
-}
+/* Definitions */
+let space = '   ';
 
+/* Declarations */
 let args;
 let output;
 let mode;
 let dictionary;
 let dictionary_interval;
 let dictionary_path;
+
+/**
+ * Prints proper command usage to console with 'Invalid syntax!' prefix.
+ * @constructor
+ * @param {string} pusg - Proper usage.
+ */
+function syntaxError(pusg) {
+    return console.log(space + 'Invalid syntax! Usage: ' + pusg);
+};
+
+/**
+ * Logs the message to console and file output.
+ * @constructor
+ * @param {string} message - Message to be logged.
+ */
+function log(message) {
+    output = output + '\n' + message;
+    console.log(message);
+};
+
+/**
+ * Saves cracker output to text file.
+ */
+function saveCrackerer() {
+    let statistics;
+    filesystem.stat('./_logs', (err, stats) => {
+        if (!err) {
+            filesystem.writeFile('./_logs/output.txt', output, (err) => {
+                console.log(space + 'Saved!');
+                rl.prompt();
+            });
+        } else if (err.code === 'ENOENT') {
+            filesystem.mkdir('./_logs/');
+            filesystem.writeFile('./_logs/output.txt', output, (err) => {
+                console.log(space + 'Saved!');
+                rl.prompt();
+            });
+        }
+    });
+};
+
+/**
+ * Starts crackerer.
+ */
+function startCrackerer() {
+    if (mode === 'profile') {
+        filesystem.readFile(dictionary_path, (err, data) => {
+            dictionary = data.toString().split('\r\n');
+            for (let index = 0; index < dictionary.length; ++index) {
+                setTimeout(() => {
+                    let value = dictionary[index];
+                    steam.resolveVanityURL({
+                        vanityurl: value,
+                        callback: (err, data) => {
+                            log(dictionary[index] + '  ' + ((!data.response.steamid) ? space + 'Not claimed!' : data.response.steamid));
+                            rl.prompt();
+                        }
+                    })
+                }, ((dictionary_interval) ? dictionary_interval : 600) * index)
+            };
+        });
+    } else if (mode === 'group') {
+        // TODO
+    }
+};
 rl.setPrompt(config.prompt);
 rl.prompt();
 rl.on('line', (input) => {
@@ -39,7 +99,7 @@ rl.on('line', (input) => {
                 return rl.prompt();
             }
             dictionary_interval = args[1];
-            console.log('    Set interval to: ' + args[1] + ' ms');
+            console.log(space + 'Set interval to: ' + args[1] + ' ms');
             rl.prompt();
             break;
         case 'path':
@@ -48,18 +108,20 @@ rl.on('line', (input) => {
                 return rl.prompt();
             }
             dictionary_path = args[1];
-            console.log('    Set dictionary path to: ' + args[1]);
+            console.log(space + 'Set dictionary path to: ' + args[1]);
             rl.prompt();
             break;
         case 'mode':
-            if(!args[1] || args[1] !== 'profile' || args[1] !== 'group') {
+            if (!args[1]) {
                 syntaxError('mode <profile/group>');
                 return rl.prompt();
             }
             mode = args[1];
+            console.log(space + 'Set mode to: ' + args[1]);
+            rl.prompt();
             break;
         case 'author':
-            console.log('\nzxvnme\ngithub.com/zxvnme/crackerer\n' + pack.version + '\n');
+            console.log('\n zxvnme\n github.com/zxvnme/crackerer\n' + ' ' + pack.version + '\n');
             rl.prompt();
             break;
         case 'clear':
@@ -71,9 +133,10 @@ rl.on('line', (input) => {
             break;
         case 'start':
             if (dictionary_path != null && mode != null) {
+                rl.prompt();
                 startCrackerer();
             } else {
-                console.log('    Cannot start because of no path or cracking mode specified :(');
+                console.log(space + 'Cannot start because of no path or cracking mode specified :(');
                 return rl.prompt();
             }
             break;
@@ -85,7 +148,7 @@ rl.on('line', (input) => {
             steam.resolveVanityURL({
                 vanityurl: args[1],
                 callback: (err, data) => {
-                    log(args[1] + ' ' + ((!data.response.steamid) ? '    Not claimed!' : data.response.steamid));
+                    log(args[1] + ' ' + ((!data.response.steamid) ? space + 'Not claimed!' : data.response.steamid));
                     rl.prompt();
                 }
             });
@@ -99,50 +162,3 @@ rl.on('line', (input) => {
             break;
     }
 });
-
-function log(message) {
-    output = output + '\n' + message;
-    console.log(message);
-}
-
-function saveCrackerer() {
-    let statistics;
-    filesystem.stat('./_logs', (err, stats) => {
-        if (!err) {
-            filesystem.writeFile('./_logs/output.txt', output, (err) => {
-                console.log('    Saved!');
-            });
-        } else if (err.code === 'ENOENT') {
-            filesystem.mkdir('./_logs/');
-        }
-    });
-}
-
-function startCrackerer() {
-    if(mode === 'profile') {
-        filesystem.readFile(dictionary_path, (err, data) => {
-            dictionary = data.toString().split('\r\n');
-            for (let index = 0; index < dictionary.length; ++index) {
-                setTimeout(() => {
-                    let value = dictionary[index];
-                    steam.resolveVanityURL({
-                        vanityurl: value,
-                        callback: (err, data) => {
-                            log(dictionary[index] + '  ' + ((!data.response.steamid) ? '   Not claimed!' : data.response.steamid));
-                            rl.prompt();
-                        }
-                    })
-                }, ((dictionary_interval) ? dictionary_interval : 600) * index)
-            };
-        });
-    } else if (mode === 'group') {
-        //TODO
-    }
-}
-
-
-
-
-
-
-
